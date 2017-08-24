f637b06cf2ae38961761e171682afb65c81ea9e7
sakshri20 <sak.20.shrivastava@gmail.com>
2017-08-24 12:18:19 -0400

    Tutorial part 1
    Basics of Swift: Constants, Variables,  and basic arithmatic operations.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,84 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+print("Hey I am Sakshi")
+2 + 6
+10 - 2
+2 * 4
+24 / 3
+22 / 7
+22.0 / 7.0
+28 % 10
+
+/*The shift left and shift right operations take the binary form of a decimal number and shift the digits left or right, respectively. Then they return the decimal form of the new binary number.*/
+
+// Shift Left
+1 << 3
+
+// Shift Right
+
+32 >> 2
+
+/* Math Functions in Swift*/
+
+sin(45 * Double.pi / 180)
+cos(135 * Double.pi / 180)
+sqrt(2.0)
+max(5, 15)
+min(-2, -1)
+max(sqrt(2.0), Double.pi/2)
+
+/*Constants in Swift*/
+
+let number: Int = 4
+let pi: Double = 3.14
+
+/*Variables in Swift*/
+
+var name: String = "Sakshi"
+var percentage: Double = 65.5
+
+/*Unicode variables in Swift*/
+
+//var : Int = -1
+
+/*Increment and Decrement*/
+
+var counter: Int = 5
+
+counter += 1
+counter -= 3
+counter *= 2
+counter /= 3
+
+
+/*Mini Excercise*/
+
+let myAge: Int = 26
+var averageAge: Double = 26.0
+averageAge = 30
+
+/*Even number get 0 as remainder when devided by 2. Even number get 1 as remainder when devided by 2*/
+let testNumber: Int = 23
+let evenOdd: Int = testNumber % 2
+
+var answer: Int = 0
+answer += 1
+answer = answer + 10
+answer *= 10
+answer >> 3
+
+
+/*Challenge*/
+
+let excercises: Int = 11
+var excercisesSolved: Int = 0
+excercisesSolved += 1
+
+
+
+
+
+
